47bf4c40a6d61c7ed1f9791a10711c3a761e2524
shrinivas12 <shrinivas63800@gmail.com>
2023-04-08 09:24:39 +0530

    first commit

---
+++ src/test/java/com/satur/Launch.java
@@ -0,0 +1,18 @@
+package com.satur;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Launch  {
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		WebDriver driver = new ChromeDriver(options);
+		driver.get("https://www.facebook.com/");
+	}
+
+}
